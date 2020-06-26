cf457ee9d6d9b9209d8f453db31f2caf1bd035e2
unknown <nikhilthakur281@gmail.com>
2020-06-26 18:18:57 +0545

    changed visitor/home.html

--- pages/views.py
+++ pages/views.py
@@ -4,7 +4,15 @@ from news.models import News, NewsType
 
 
 def homepage(request):
-    return render(request, 'visitor/home.html', {})
+    news_types = NewsType.objects.all()
+
+    news_types_with_news = {}
+    for items in NewsType.objects.all():
+        news_types_with_news[items.title] = items.news.all().order_by(
+            '-pub_date')[:5]
+    return render(request, 'visitor/home.html', {
+        'news_types': news_types,
+        'news_types_with_news': news_types_with_news})
 
 
 def newsPage(request, newstype):
