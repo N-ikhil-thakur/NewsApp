fd80f3bf7d99891ff260c6504001787908d9c3d0
unknown <nikhilthakur281@gmail.com>
2020-06-27 23:24:54 +0545

    changes news.html

--- pages/views.py
+++ pages/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 
 from news.models import News, NewsType
 
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+
 
 def homepage(request):
     news_types = NewsType.objects.all()
@@ -19,5 +21,17 @@ def homepage(request):
 def newsPage(request, newstype):
     news_types = NewsType.objects.all()
     newstype = NewsType.objects.get(title=newstype)
+    newses_list = News.objects.filter(news_type=newstype)
+    page = request.GET.get('page', 1)
+
+    paginator = Paginator(newses_list, 9)
+
+    try:
+        newses = paginator.page(page)
+    except PageNotAnInteger:
+        newses = paginator.page(1)
+    except EmptyPage:
+        newses = paginator.page(paginator.num_pages)
+
     return render(request, 'visitor/news.html', {
-        'newstype': newstype, 'page_type': newstype, 'news_types': news_types})
+        'newstype': newstype, 'page_type': newstype, 'news_types': news_types, 'newses': newses})
--- static/js/home.js
+++ static/js/home.js
@@ -1,1 +1,11 @@
-
+function updateDateTime() {
+  document.querySelector(
+    ".date"
+  ).innerHTML = `${new Date().toLocaleDateString()}`;
+  setInterval(() => {
+    document.querySelector(
+      ".time"
+    ).textContent = `${new Date().toLocaleTimeString()}`;
+  }, 1000);
+}
+updateDateTime();
