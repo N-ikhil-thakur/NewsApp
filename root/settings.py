ebd29cd6c39befa95239c249eb74dc8e659f59fc
unknown <nikhilthakur281@gmail.com>
2020-07-10 21:35:39 +0545

    .

--- root/settings.py
+++ root/settings.py
@@ -138,7 +138,7 @@ STATICFILES_DIRS = [
 ]
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
