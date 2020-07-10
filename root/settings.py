5cf1327e18c256c91cb037c4b0ce130a42630d36
unknown <nikhilthakur281@gmail.com>
2020-07-10 10:28:43 +0545

    .

--- root/settings.py
+++ root/settings.py
@@ -137,7 +137,7 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
 
-MEDIA_URL = '/images/'
+MEDIA_URL = '/static/images/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'staticfiles/images')
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
