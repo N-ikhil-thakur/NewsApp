76f2cd6b377857aa20e365bd63e65cc0eedb7bf0
unknown <nikhilthakur281@gmail.com>
2020-07-10 22:16:21 +0545

    .

--- root/settings.py
+++ root/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'z+v1+7+z4sg@r62#nu3n+&+%g$^exwbq=v=j_i)!-9-)oh#ad9'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
