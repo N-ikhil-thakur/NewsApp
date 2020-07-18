f95c0451c6198bedbe04cbf002f1c9745d8019e9
unknown <nikhilthakur281@gmail.com>
2020-07-18 14:40:28 +0545

    added navabar sync

--- assets/js/home.js
+++ assets/js/home.js
@@ -10,3 +10,15 @@ function updateDateTime() {
   document.querySelector(".copy_year").innerHTML = new Date().getFullYear();
 }
 updateDateTime();
+
+
+let total = document.querySelectorAll(".nbar-itms").length+1;
+let scroller = document.querySelector(".nbr-bx");
+let currentElem = document.querySelector(".selected") 
+let current = currentElem.getAttribute('data-num')-1;
+
+
+scroller.scrollTo({
+left: currentElem.offsetWidth*current,
+behaviour:'auto'
+}) 
